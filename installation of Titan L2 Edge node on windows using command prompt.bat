cbe19b82efa4192bdfc12073c93f956e489af43d
Psychead <vfvdy780@gmail.com>
2025-01-29 02:08:12 +0530

    Update installation of Titan L2 Edge node on windows using command prompt.bat

--- installation of Titan L2 Edge node on windows using command prompt.bat
+++ installation of Titan L2 Edge node on windows using command prompt.bat
@@ -85,10 +85,19 @@ echo color %info_color% >> C:\titan-edge\continue_installation.bat
 echo echo "Continuing installation..." >> C:\titan-edge\continue_installation.bat
 echo cd C:\titan-edge\titan-edge_v0.1.20_246b9dd_widnows_amd64 >> C:\titan-edge\continue_installation.bat
 echo start /b titan-edge daemon start --init --url https://cassini-locator.titannet.io:5000/rpc/v0 >> C:\titan-edge\continue_installation.bat
+echo echo "Daemon started. Waiting for registration..." >> C:\titan-edge\continue_installation.bat
+echo :check_daemon >> C:\titan-edge\continue_installation.bat
+echo for /f "delims=" %%i in ('titan-edge daemon status') do set output=%%i >> C:\titan-edge\continue_installation.bat
+echo echo %output% >> C:\titan-edge\continue_installation.bat
+echo echo Checking for "Edge registered successfully, waiting for the tasks" message... >> C:\titan-edge\continue_installation.bat
+echo echo %output% | findstr /i "Edge registered successfully, waiting for the tasks" >nul && goto bind_identity_code >> C:\titan-edge\continue_installation.bat
 echo timeout /t 5 >> C:\titan-edge\continue_installation.bat
-echo echo "Daemon started. Please enter your identity code." >> C:\titan-edge\continue_installation.bat
+echo goto check_daemon >> C:\titan-edge\continue_installation.bat
+echo :bind_identity_code >> C:\titan-edge\continue_installation.bat
+echo echo "Daemon registered. Please enter your identity code." >> C:\titan-edge\continue_installation.bat
 echo set /p identity_code="Enter your identity code (hash): " >> C:\titan-edge\continue_installation.bat
 echo titan-edge bind --hash=%identity_code% https://api-test1.container1.titannet.io/api/v2/device/binding >> C:\titan-edge\continue_installation.bat
+echo echo "Node is running and bound to your account." >> C:\titan-edge\continue_installation.bat
 echo rmdir /s /q C:\titan-edge >> C:\titan-edge\continue_installation.bat
 echo del C:\titan-edge.zip >> C:\titan-edge\continue_installation.bat
 echo echo Installation complete! >> C:\titan-edge\continue_installation.bat
